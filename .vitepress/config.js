35c8a685ce684791c3418b4070060b8aec27bae2
codering <zcodeworld@gmail.com>
2023-07-15 17:04:15 +0800

    update config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,45 +1,45 @@
-import { defineConfig } from 'vitepress'
-import { getPosts } from './theme/serverUtils'
+import { defineConfig } from "vitepress";
+import { getPosts } from "./theme/serverUtils";
 
 //每页的文章数量
-const pageSize = 10
+const pageSize = 10;
 
 export default defineConfig({
-    title: 'vitepress',
-    base: '/',
-    cacheDir: './node_modules/vitepress_cache',
-    description: 'vitepress,blog,blog-theme',
-    ignoreDeadLinks: true,
-    themeConfig: {
-        posts: await getPosts(pageSize),
-        website: 'https://github.com/airene/vitepress-blog-pure', //copyright link
-        // 评论的仓库地址
-        comment: {
-            repo: 'airene/vitepress-blog-pure',
-            themes: 'github-light',
-            issueTerm: 'pathname'
-        },
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Archives', link: '/pages/archives' },
-            { text: 'Tags', link: '/pages/tags' },
-            { text: 'About', link: '/pages/about' }
-            // { text: 'Airene', link: 'http://airene.net' }  -- External link test
-        ],
-
-        //outline:[2,3],
-        outlineTitle: '文章摘要',
-        socialLinks: [{ icon: 'github', link: 'https://github.com/airene/vitepress-blog-pure' }]
+  title: "tcptty",
+  base: "/blog",
+  cacheDir: "./node_modules/vitepress_cache",
+  description: "vitepress,blog,blog-theme",
+  ignoreDeadLinks: true,
+  themeConfig: {
+    posts: await getPosts(pageSize),
+    website: "https://github.com/codering/blog", //copyright link
+    // 评论的仓库地址
+    comment: {
+      repo: "codering/blog",
+      themes: "github-light",
+      issueTerm: "pathname",
     },
-    srcExclude: ['README.md'], // exclude the README.md , needn't to compiler
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Archives", link: "/pages/archives" },
+      { text: "Tags", link: "/pages/tags" },
+      { text: "About", link: "/pages/about" },
+      // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+    ],
 
-    vite: {
-        //build: { minify: false }
-        server: { port: 5000 }
-    }
-    /*
+    //outline:[2,3],
+    outlineTitle: "文章摘要",
+    socialLinks: [{ icon: "github", link: "https://github.com/codering/blog" }],
+  },
+  srcExclude: ["README.md"], // exclude the README.md , needn't to compiler
+
+  vite: {
+    //build: { minify: false }
+    server: { port: 5000 },
+  },
+  /*
       optimizeDeps: {
           keepNames: true
       }
       */
-})
+});
